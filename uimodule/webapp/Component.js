86e18b2725927fb317d08a14e8489c028b32d6be
pamesty <p.amesty.guanipa@accenture.com>
2021-08-30 17:10:26 -0300

    midColumnPages y routing agregados

--- uimodule/webapp/Component.js
+++ uimodule/webapp/Component.js
@@ -1,6 +1,12 @@
 sap.ui.define(
-    ["sap/ui/core/UIComponent", "sap/ui/Device", "com/te/pollutionTracker/model/models"],
-    function(UIComponent, Device, models) {
+    [
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "com/te/pollutionTracker/model/models",
+        "sap/ui/model/json/JSONModel",
+        "sap/f/library"
+    ],
+    function(UIComponent, Device, models, JSONModel, fioriLibrary) {
         "use strict";
 
         return UIComponent.extend("com.te.pollutionTracker.Component", {
@@ -16,12 +22,26 @@ sap.ui.define(
             init: function() {
                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
+                var oModel = new JSONModel();
+                this.setModel(oModel);
+                // // enable routing
+                var oRouter = this.getRouter();
+                oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+                oRouter.initialize();
 
-                // enable routing
-                //this.getRouter().initialize();
+                // // set the device model
+                // this.setModel(models.createDeviceModel(), "device");
+            },
+            _onBeforeRouteMatched: function(oEvent) {
+                var oModel = this.getModel(),
+                    sLayout = oEvent.getParameters().arguments.layout;
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
+                // If there is no layout parameter, set a default layout (normally OneColumn)
+                if (!sLayout) {
+                    sLayout = fioriLibrary.LayoutType.OneColumn;
+                }
+
+                oModel.setProperty("/layout", sLayout);
             }
         });
     }
--- uimodule/webapp/controller/Master.controller.js
+++ uimodule/webapp/controller/Master.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
         "com/te/pollutionTracker/controller/BaseController",
-        "sap/ui/model/json/JSONModel"
+        "sap/ui/model/json/JSONModel",
+        "sap/f/library"
     ],
-    function(Controller, JSONModel) {
+    function(Controller, JSONModel, fioriLibrary) {
         "use strict";
 
         return Controller.extend("com.te.pollutionTracker.controller.Master", {
@@ -10,6 +11,13 @@ sap.ui.define([
                 let countriesModel = new JSONModel('https://02a7a2dc-bd4b-4564-ac9e-4a01fa796d0b.mock.pstmn.io/countries');
                 countriesModel.setSizeLimit(1000);
                 this.getView().setModel(countriesModel, 'countries');
+            },
+
+            onCountryPress: function() {
+                //obtiene el Flexible Column Layout
+                let getFCLRef = this.getView().getParent().getParent();
+
+                getFCLRef.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
             }
         });
     });
