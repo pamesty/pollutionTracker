f93935a3a38a19e1bfa97954718cf4250f2a51bd
pamesty <p.amesty.guanipa@accenture.com>
2021-08-31 15:12:31 -0300

    Checkear el Maste.view.xml y el Master.controller.js ya que la data no se está cargando

--- uimodule/webapp/controller/Detail.controller.js
+++ uimodule/webapp/controller/Detail.controller.js
@@ -1,15 +1,46 @@
 sap.ui.define([
-        "com/te/pollutionTracker/controller/BaseController",
-        "sap/ui/model/json/JSONModel"
-    ],
-    function(Controller, JSONModel) {
-        "use strict";
+    "com/te/pollutionTracker/controller/BaseController",
+    'sap/ui/model/json/JSONModel',
+    'sap/f/library'
+], function(Controller, JSONModel, fioriLibrary) {
+    "use strict";
 
-        return Controller.extend("com.te.pollutionTracker.controller.Detail", {
-            onInit: function() {
-                let countriesModel = new JSONModel('https://02a7a2dc-bd4b-4564-ac9e-4a01fa796d0b.mock.pstmn.io/countries');
-                countriesModel.setSizeLimit(1000);
-                this.getView().setModel(countriesModel, 'countries');
-            }
-        });
+    return Controller.extend("com.te.pollutionTracker.controller.Detail", {
+        onInit: function() {
+            var oOwnerComponent = this.getOwnerComponent();
+
+            this.oRouter = oOwnerComponent.getRouter();
+            this.oModel = oOwnerComponent.getModel();
+
+            this.oRouter.getRoute("master").attachPatternMatched(this._onSelectingCountry, this);
+            this.oRouter.getRoute("detail").attachPatternMatched(this._onSelectingCountry, this);
+        },
+
+        _onSelectingCountry: function(oEvent) {
+            this._country = oEvent.getParameter("arguments").country || this._country || "0";
+
+        },
+        onStatePress: function(oEvent) {
+            this.loadData();
+            this.oRouter.navTo("city", { layout: fioriLibrary.LayoutType.ThreeColumnsMidExpanded, state: state, country: this._country });
+        },
+        onEditToggleButtonPress: function() {
+            var oObjectPage = this.getView().byId("ObjectPageLayout"),
+                bCurrentShowFooterState = oObjectPage.getShowFooter();
+
+            oObjectPage.setShowFooter(!bCurrentShowFooterState);
+        },
+
+        onExit: function() {
+            this.oRouter.getRoute("master").detachPatternMatched(this._onSelectingCountry, this);
+            this.oRouter.getRoute("detail").detachPatternMatched(this._onSelectingCountry, this);
+        },
+
+        loadData: async function() {
+            var state = oEvent.getSource().getBindingContext("states").getObject().state;
+            var stateModel = await new JSONModel('https://02a7a2dc-bd4b-4564-ac9e-4a01fa796d0b.mock.pstmn.io/state?country=' + this._country);
+            stateModel.setSizeLimit(1000);
+            this.getView().setModel(stateModel, 'states');
+        }
     });
+});
--- uimodule/webapp/controller/Master.controller.js
+++ uimodule/webapp/controller/Master.controller.js
@@ -1,23 +1,21 @@
 sap.ui.define([
-        "com/te/pollutionTracker/controller/BaseController",
-        "sap/ui/model/json/JSONModel",
-        "sap/f/library"
-    ],
-    function(Controller, JSONModel, fioriLibrary) {
-        "use strict";
+    "com/te/pollutionTracker/controller/BaseController",
+    'sap/ui/model/json/JSONModel',
+    'sap/f/library'
+], function(Controller, JSONModel, fioriLibrary) {
+    "use strict";
 
-        return Controller.extend("com.te.pollutionTracker.controller.Master", {
-            onInit: function() {
-                let countriesModel = new JSONModel('https://02a7a2dc-bd4b-4564-ac9e-4a01fa796d0b.mock.pstmn.io/countries');
-                countriesModel.setSizeLimit(1000);
-                this.getView().setModel(countriesModel, 'countries');
-            },
+    return Controller.extend("com.te.pollutionTracker.controller.Master", {
 
-            onCountryPress: function() {
-                //obtiene el Flexible Column Layout
-                let getFCLRef = this.getView().getParent().getParent();
-
-                getFCLRef.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
-            }
-        });
+        onInit: function() {
+            var countriesModel = new JSONModel('https://02a7a2dc-bd4b-4564-ac9e-4a01fa796d0b.mock.pstmn.io/countries');
+            countriesModel.setSizeLimit(1000);
+            this.getView().setModel(countriesModel, 'countries');
+            this.oRouter = this.getOwnerComponent().getRouter();
+        },
+        onCountryPress: function(oEvent) {
+            var country = oEvent.getSource().getBindingContext("countries").getObject().country;
+            this.oRouter.navTo("detail", { layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded, country: country });
+        }
     });
+});
